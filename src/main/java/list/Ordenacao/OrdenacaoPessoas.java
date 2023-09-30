f6ba9cae839a3bf3a64627f3653d2a559a431b94
reissjoao <joaooreis@hotmail.com>
2023-09-29 23:06:34 -0300

    Adicionando ordenacao com Comparator e Comparable
    Signed-off-by: reissjoao <joaooreis@hotmail.com>

---
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,43 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoas(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoa = new OrdenacaoPessoas();
+        ordenacaoPessoa.adicionarPessoas("Nome 1", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoas("Nome 2", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoas("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoas("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+
+
+}
